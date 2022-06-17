54477eef2b941d2b4cae802b0569f34dd9bd313d
KidChaos <genesis.enedeh@gmail.com>
2022-06-17 23:10:04 +0100

    Put in user model

--- IBC/User/models.py
+++ IBC/User/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    RANK_BOOK_WORM = 'BW'
+    RANK_GOLD_BOOK_WORM = 'GBW'
+    RANK_PLATINUM_BOOK_WORM = 'PBW'
+    RANK_LIST = [
+        (RANK_BOOK_WORM, 'Bookworm'),
+        (RANK_GOLD_BOOK_WORM, 'Gold Bookworm'),
+        (RANK_PLATINUM_BOOK_WORM, 'Platinum Bookworm')
+    ]
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=8, unique=True)
+    email = models.EmailField(unique=True)
+    is_writer = models.BooleanField()
+    rank = models.CharField(max_length=3, choices=RANK_LIST)
+    bio = models.TextField(max_length=500, help_text="MAX LENGTH OF 500")
+
+    def __str__(self):
+        return self.username
