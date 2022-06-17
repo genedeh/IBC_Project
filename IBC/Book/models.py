7f46570840ea3a3756c7a3a1d729df2f485057fa
KidChaos <genesis.enedeh@gmail.com>
2022-06-17 23:51:01 +0100

    add in book model

--- IBC/Book/models.py
+++ IBC/Book/models.py
@@ -1,3 +1,20 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from User.models import User
+
 
 # Create your models here.
+class Book(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField()
+    created = models.DateTimeField(auto_now_add=True)
+    story = models.TextField()
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(1)])
+    likes = models.IntegerField()
+
+    def __str__(self):
+        return self.title
