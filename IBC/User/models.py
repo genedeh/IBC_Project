c5997df7341b2234e25e28f3580468cb4de46263
KidChaos <genesis.enedeh@gmail.com>
2022-06-18 10:28:13 +0100

    change user model to abstract user class model

--- IBC/User/models.py
+++ IBC/User/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class User(models.Model):
+class User(AbstractUser):
     RANK_BOOK_WORM = 'BW'
     RANK_GOLD_BOOK_WORM = 'GBW'
     RANK_PLATINUM_BOOK_WORM = 'PBW'
@@ -11,12 +12,10 @@ class User(models.Model):
         (RANK_GOLD_BOOK_WORM, 'Gold Bookworm'),
         (RANK_PLATINUM_BOOK_WORM, 'Platinum Bookworm')
     ]
-    username = models.CharField(max_length=20, unique=True)
-    password = models.CharField(max_length=8, unique=True)
     email = models.EmailField(unique=True)
-    is_writer = models.BooleanField()
-    rank = models.CharField(max_length=3, choices=RANK_LIST)
-    bio = models.TextField(max_length=500, help_text="MAX LENGTH OF 500")
+    is_writer = models.BooleanField(default=False)
+    rank = models.CharField(max_length=3, choices=RANK_LIST, default=RANK_BOOK_WORM)
+    bio = models.TextField(max_length=500, help_text="MAX LENGTH OF 500", default="NO BIO")
 
     def __str__(self):
         return self.username
